d98567f03b5e4873a044856fad6a573170749329
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2026-02-25 20:06:22 -0300

    feat: add header, initial search input and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between py-6">
+      <Image src={"/logo.svg"} alt="Aparatus" width={100} height={26.09} />
+
+      <Button variant={"outline"} size={"icon"}>
+        <MenuIcon />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
