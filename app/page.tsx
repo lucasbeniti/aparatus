cbb3add598efbb283849b08f3f811b3675b61359
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2026-02-25 20:34:12 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const Home = () => {
   return (
-    <div className="px-5">
+    <main className="px-5">
       <Header />
 
       <div className="space-y-4">
@@ -17,8 +18,19 @@ const Home = () => {
           sizes="100vw"
           className="h-auto w-full"
         />
+
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/7e309eaa-d722-465b-b8b6-76217404a3d3-16s.png"
+          date={new Date()}
+        />
       </div>
-    </div>
+    </main>
   );
 };
 
